a7578e738c7eb8380feec4261a5616cbd46edfe3
charmin16 <charming_princewill@yahoo.com>
2023-01-03 10:39:31 +0100

    slight modification

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import './App.css';
 import Tours from './Tours/Tours';
 
+// This is a very beautiful project 
+// So so proud of the efforts I have put into this project
+
 function App() {
   return (
     <div className="App">
